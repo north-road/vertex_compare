c40220f078bfd59b92fb9556c061a40b2c9b4ca9
Nyall Dawson <nyall.dawson@gmail.com>
2021-03-30 12:37:58 +1000

    Use text renderer for vertex numbers

--- vertex_compare/core/vertex_highlighter.py
+++ vertex_compare/core/vertex_highlighter.py
@@ -17,7 +17,8 @@ from typing import Optional
 
 from qgis.PyQt.QtCore import (
     QPointF,
-    Qt
+    Qt,
+    QRectF
 )
 from qgis.PyQt.QtGui import (
     QColor
@@ -33,12 +34,68 @@ from qgis.core import (
     QgsFields,
     QgsMarkerLineSymbolLayer,
     QgsMarkerSymbol,
-    QgsFontMarkerSymbolLayer,
     QgsSymbolLayer,
     QgsProperty,
     QgsMarkerSymbolLayer,
-    QgsSimpleMarkerSymbolLayer
+    QgsSimpleMarkerSymbolLayer,
+    QgsSymbolRenderContext,
+    QgsTextFormat,
+    QgsTextRenderer,
+    QgsRenderContext,
+    QgsFeature,
+    QgsUnitTypes
 )
+
+
+class TextRendererMarkerSymbolLayer(QgsMarkerSymbolLayer):
+    """
+    A marker symbol layer which uses QgsTextRenderer to draw text
+    """
+
+    def __init__(self, text_format: QgsTextFormat):
+        super().__init__()
+        self.text_format = text_format
+        self.vertex_id = 1
+
+    def layerType(self) -> str:  # pylint: disable=missing-function-docstring
+        return 'TextRenderer'
+
+    def startRender(self, context: QgsSymbolRenderContext):  # pylint: disable=missing-function-docstring,unused-argument
+        self.vertex_id = 1
+
+    def stopRender(self, context: QgsSymbolRenderContext):  # pylint: disable=missing-function-docstring,unused-argument
+        self.vertex_id = 1
+
+    def usedAttributes(self, context: QgsRenderContext):  # pylint: disable=missing-function-docstring
+        return self.text_format.referencedFields(context)
+
+    def setColor(self, color):  # pylint: disable=missing-function-docstring
+        self.text_format.setColor(color)
+
+    def startFeatureRender(self, feature: QgsFeature, context: QgsRenderContext):  # pylint: disable=missing-function-docstring
+        self.vertex_id = 1
+        super().startFeatureRender(feature, context)
+
+    def renderPoint(self, point: QPointF, context: QgsSymbolRenderContext):  # pylint: disable=missing-function-docstring
+        if not context.renderContext().painter():
+            return
+
+        # offset point a little
+        offset = context.renderContext().convertToPainterUnits(1, QgsUnitTypes.RenderMillimeters)
+        render_point = QPointF(point.x() + offset, point.y() - offset)
+
+        QgsTextRenderer.drawText(render_point, 0, QgsTextRenderer.AlignLeft,
+                                 [str(self.vertex_id)], context.renderContext(), self.text_format)
+        self.vertex_id += 1
+
+    def clone(self):  # pylint: disable=missing-function-docstring
+        return TextRendererMarkerSymbolLayer(self.text_format)
+
+    def properties(self):  # pylint: disable=missing-function-docstring
+        return {}
+
+    def bounds(self, point: QPointF, context: QgsSymbolRenderContext):  # pylint: disable=missing-function-docstring,unused-argument
+        return QRectF()
 
 
 class VertexHighlighterRenderer(QgsSingleSymbolRenderer):
@@ -46,12 +103,23 @@ class VertexHighlighterRenderer(QgsSingleSymbolRenderer):
     Custom layer renderer which highlights vertices in selected features only
     """
 
+    COLORS = [
+        QColor(255, 0, 0),
+        QColor(0, 150, 0),
+        QColor(0, 0, 255),
+    ]
+
     def __init__(self, symbol: QgsSymbol, selection: list):
         super().__init__(symbol)
-        self.selection = selection
+        self.selection = sorted(selection)
+        self.feature_index = 0
 
     def filter(self, _=QgsFields()) -> str:  # pylint: disable=missing-function-docstring
         return f'$id in ({",".join([str(i) for i in self.selection])})'
+
+    def startRender(self, context: QgsRenderContext, fields: QgsFields):  # pylint: disable=missing-function-docstring
+        self.feature_index = 0
+        super().startRender(context, fields)
 
     def renderFeature(self,  # pylint: disable=missing-function-docstring
                       feature,
@@ -67,6 +135,11 @@ class VertexHighlighterRenderer(QgsSingleSymbolRenderer):
 
         if feature.id() not in self.selection:
             return False
+
+        color = VertexHighlighterRenderer.COLORS[self.feature_index % len(VertexHighlighterRenderer.COLORS)]
+        self.feature_index += 1
+
+        self.symbol().setColor(color)
 
         return super().renderFeature(feature, context, layer, False, drawVertexMarker)
 
@@ -104,17 +177,20 @@ class VertexHighlighterRendererGenerator(QgsFeatureRendererGenerator):
         simple_marker = QgsSimpleMarkerSymbolLayer(QgsSimpleMarkerSymbolLayer.Circle)
         simple_marker.setSize(1)
         simple_marker.setStrokeStyle(Qt.NoPen)
+        # not so nice, but required to allow us to dynamically change this color mid-way through rendering
+        simple_marker.setDataDefinedProperty(QgsSymbolLayer.PropertyFillColor, QgsProperty.fromValue(None))
         vertex_marker_symbol.changeSymbolLayer(0, simple_marker)
 
-        font_marker = QgsFontMarkerSymbolLayer()
-        font_marker.setFontFamily('Arial')
-        font_marker.setColor(QColor(255, 0, 0))
-        font_marker.setDataDefinedProperty(QgsSymbolLayer.PropertyCharacter,
-                                           QgsProperty.fromExpression('@geometry_point_num'))
-        font_marker.setHorizontalAnchorPoint(QgsMarkerSymbolLayer.Left)
-        font_marker.setVerticalAnchorPoint(QgsMarkerSymbolLayer.Bottom)
-        font_marker.setOffset(QPointF(1, -1))
+        text_format = QgsTextFormat()
+        text_format.setColor(QColor(255, 0, 0))
+        text_format.setSize(10)
+        text_format.setNamedStyle('Bold')
+        text_format.buffer().setEnabled(True)
+        text_format.buffer().setColor(QColor(255, 255, 255))
+
+        font_marker = TextRendererMarkerSymbolLayer(text_format)
         vertex_marker_symbol.appendSymbolLayer(font_marker)
+
         marker_line.setSubSymbol(vertex_marker_symbol)
 
         if self.layer_type == QgsWkbTypes.LineGeometry:
